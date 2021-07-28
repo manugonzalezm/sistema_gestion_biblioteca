595f1f079ff6bb3ba4653dd9c3df58cfc8220fe3
manugonzalezm <manuelgonzalez4a@gmail.com>
2021-07-28 02:09:03 -0300

    Finalizacion del proyecto

--- app.py
+++ app.py
@@ -2,17 +2,16 @@ from logging import debug
 from flask import Flask
 from flask import render_template,request,redirect,url_for  # agregamos request  
 from flaskext.mysql import MySQL    # para comunicacion mysql
-from datetime import datetime       #para darle nombre a las fotos
 from flask import send_from_directory
 import os #para acceder a los archivos
 
 app=Flask(__name__)     # creacion del objeto app de la clase Flask
 
 mysql = MySQL()     # creacion de un objeto mysql de la clase MySQL
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'biblioteca'     # nombre de la base de datos
+app.config['MYSQL_DATABASE_HOST'] = 'remotemysql.com'
+app.config['MYSQL_DATABASE_USER'] = 'yRsT5S18UV'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'npWRLL9PcG'
+app.config['MYSQL_DATABASE_DB'] = 'yRsT5S18UV'     # nombre de la base de datos
 mysql.init_app(app)     # Llamamos al metodo init_app de mysql con el objeto de flask como parametro
 
 CARPETA=os.path.join('uploads')     #referencia a la carpeta
@@ -28,7 +27,7 @@ def index():
 
 @app.route('/inventario')
 def inventario():
-    sql="SELECT `inventario`.`id_stock`, `inventario`.`nombre_libro`, `categorias`.`categoria`, `inventario`.`autor`, `inventario`.`url_foto` FROM `biblioteca`.`inventario` INNER JOIN `categorias` ON `inventario`.`id_categoria` = `categorias`.`id_categoria` WHERE `inventario`.`disponibilidad`=1;"
+    sql="SELECT `inventario`.`id_stock`, `inventario`.`nombre_libro`, `categorias`.`categoria`, `inventario`.`autor`, `inventario`.`url_foto` FROM `inventario` INNER JOIN `categorias` ON `inventario`.`id_categoria` = `categorias`.`id_categoria` WHERE `inventario`.`disponibilidad`=1;"
     conn=mysql.connect()        # hacemos la conexion a mysql
     cursor=conn.cursor()
     cursor.execute(sql)     # ejecucion del string sql
@@ -48,7 +47,7 @@ def inventario():
 
 @app.route('/prestamos')
 def prestamos():
-    sql="SELECT `inventario`.`id_stock`, `inventario`.`nombre_libro`, `socios`.`nombre_socio`, `inventario`.`fecha_devolucion`, `inventario`.`url_foto` FROM `biblioteca`.`inventario` INNER JOIN `socios` ON `inventario`.`id_socio` = `socios`.`id_socio` WHERE `inventario`.`disponibilidad`=0;"
+    sql="SELECT `inventario`.`id_stock`, `inventario`.`nombre_libro`, `socios`.`nombre_socio`, `inventario`.`fecha_devolucion`, `inventario`.`url_foto` FROM `inventario` INNER JOIN `socios` ON `inventario`.`id_socio` = `socios`.`id_socio` WHERE `inventario`.`disponibilidad`=0;"
     conn=mysql.connect()        # hacemos la conexion a mysql
     cursor=conn.cursor()
     cursor.execute(sql)     # ejecucion del string sql
@@ -79,7 +78,7 @@ def devolver_libro(id):
 def devolver():
     id=request.form['txtID'];
 
-    sql = "UPDATE `biblioteca`.`inventario` SET `id_socio`=null, `disponibilidad`=1, `fecha_devolucion`=null WHERE (`id_stock`=%s);"
+    sql = "UPDATE `inventario` SET `id_socio`=null, `disponibilidad`=1, `fecha_devolucion`=null WHERE (`id_stock`=%s);"
     datos=(id)
     conn=mysql.connect()
     cursor=conn.cursor()
@@ -92,7 +91,7 @@ def devolver():
 def socios():
     conn = mysql.connect()   #se conecta a la conexion mysql.init_app(app)
     cursor = conn.cursor()   #almacena lo que ejecutamos
-    cursor.execute("SELECT * FROM `biblioteca`.`socios`")    #ejecuta la sentencia SQL sobre el id
+    cursor.execute("SELECT * FROM `socios`")    #ejecuta la sentencia SQL sobre el id
     socios=cursor.fetchall()    #traemos toda la informacion
     conn.commit()    #cerramos la conexion
     return render_template('biblioteca/socios.html', socios=socios)
@@ -112,7 +111,7 @@ def storage_libro():
         nuevoNombreFoto=_foto.filename   #concatena el nombre
         _foto.save("uploads/"+nuevoNombreFoto)  #lo guarda en la carpeta
 
-    sql = "INSERT INTO `biblioteca`.`inventario` (`id_stock`,`nombre_libro`,`id_categoria`, `autor`, `url_foto`, `disponibilidad`) VALUES (null,%s,%s,%s,%s,1);"
+    sql = "INSERT INTO `inventario` (`id_stock`,`nombre_libro`,`id_categoria`, `autor`, `url_foto`, `disponibilidad`) VALUES (null,%s,%s,%s,%s,1);"
     datos = (_nombre,_categoria,_autor,nuevoNombreFoto)
     conn=mysql.connect()
     cursor=conn.cursor()
@@ -125,13 +124,13 @@ def storage_libro():
 def prestar(id):
     conn = mysql.connect()   #se conecta a la conexion mysql.init_app(app)
     cursor = conn.cursor()   #almacena lo que ejecutamos
-    cursor.execute("SELECT * FROM `biblioteca`.`inventario` WHERE id_stock=%s", (id))     #ejecuta la sentencia SQL sobre el id
+    cursor.execute("SELECT * FROM `inventario` WHERE id_stock=%s", (id))     #ejecuta la sentencia SQL sobre el id
     libros=cursor.fetchall()    #traemos toda la informacion
     conn.commit()    #cerramos la conexion
 
     conn = mysql.connect()   #se conecta a la conexion mysql.init_app(app)
     cursor = conn.cursor()   #almacena lo que ejecutamos
-    cursor.execute("SELECT * FROM `biblioteca`.`socios`")    #ejecuta la sentencia SQL sobre el id
+    cursor.execute("SELECT * FROM `socios`")    #ejecuta la sentencia SQL sobre el id
     socios=cursor.fetchall()    #traemos toda la informacion
     conn.commit()    #cerramos la conexion
     return render_template('biblioteca/new_prestamo.html', libros=libros, socios=socios)
@@ -142,7 +141,7 @@ def storage_prestamo():
     _fecha = request.form['txtFecha']
     _idLibro = request.form['txtIdLibro']
 
-    sql = "UPDATE `biblioteca`.`inventario` SET `id_socio`=%s, `fecha_devolucion`=%s, `disponibilidad`='0' WHERE id_stock=%s;"
+    sql = "UPDATE `inventario` SET `id_socio`=%s, `fecha_devolucion`=%s, `disponibilidad`='0' WHERE id_stock=%s;"
     datos=(id,_fecha,_idLibro)
 
     conn=mysql.connect()
@@ -161,7 +160,7 @@ def storage_socio():
     _nombre = request.form['txtNombre']     #toma los datos que envio el form en txtNombre
     _correo = request.form['txtCorreo']     #toma los datos del form
 
-    sql = "INSERT INTO `biblioteca`.`socios` (`id_socio`,`nombre_socio`,`correo_socio`) VALUES (null,%s,%s);"
+    sql = "INSERT INTO `socios` (`id_socio`,`nombre_socio`,`correo_socio`) VALUES (null,%s,%s);"
     datos = (_nombre,_correo)
     conn=mysql.connect()
     cursor=conn.cursor()
@@ -255,11 +254,11 @@ def destroy_libro(id):
     conn = mysql.connect()   #se conecta a la conexion mysql.init_app(app)
     cursor = conn.cursor()   #almacena lo que ejecutamos
 
-    cursor.execute("SELECT url_foto FROM `biblioteca`.`inventario` WHERE id_stock=%s",(id))
+    cursor.execute("SELECT url_foto FROM `inventario` WHERE id_stock=%s",(id))
     fila=cursor.fetchall()
     os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
 
-    cursor.execute("DELETE FROM `biblioteca`.`inventario` WHERE id_stock=%s", (id))   #en vez de pasarle el string la escribimos
+    cursor.execute("DELETE FROM `inventario` WHERE id_stock=%s", (id))   #en vez de pasarle el string la escribimos
     conn.commit()      #cerramos la conexion
     return redirect('/inventario')    #regresamos de donde vinimos
 
@@ -267,7 +266,7 @@ def destroy_libro(id):
 def edit_libro(id):
     conn = mysql.connect()   #se conecta a la conexion mysql.init_app(app)
     cursor = conn.cursor()   #almacena lo que ejecutamos
-    cursor.execute("SELECT `inventario`.`id_stock`, `inventario`.`nombre_libro`, `inventario`.`autor`, `inventario`.`url_foto` FROM `biblioteca`.`inventario` WHERE id_stock=%s", (id))     #ejecuta la sentencia SQL sobre el id
+    cursor.execute("SELECT `inventario`.`id_stock`, `inventario`.`nombre_libro`, `inventario`.`autor`, `inventario`.`url_foto` FROM `inventario` WHERE id_stock=%s", (id))     #ejecuta la sentencia SQL sobre el id
     libros=cursor.fetchall()    #traemos toda la informacion
     conn.commit()    #cerramos la conexion
     return render_template('biblioteca/edit_libro.html', libros=libros)
@@ -279,7 +278,7 @@ def update():
     _foto=request.files['txtFoto']
     id=request.form['txtID']
 
-    sql = "UPDATE `biblioteca`.`inventario` SET `nombre_libro`=%s, `autor`=%s WHERE id_stock=%s;"
+    sql = "UPDATE `inventario` SET `nombre_libro`=%s, `autor`=%s WHERE id_stock=%s;"
     datos=(_nombre,_autor,id)
 
     conn=mysql.connect()
@@ -289,11 +288,11 @@ def update():
         nuevoNombreFoto=_foto.filename
         _foto.save("uploads/"+nuevoNombreFoto)
 
-        cursor.execute("SELECT url_foto FROM `biblioteca`.`inventario` WHERE id_stock=%s",(id))
+        cursor.execute("SELECT url_foto FROM `inventario` WHERE id_stock=%s",(id))
         fila=cursor.fetchall()
 
         os.remove(os.path.join(app.config['CARPETA'],fila[0][0]))
-        cursor.execute("UPDATE `biblioteca`.`inventario` SET url_foto=%s WHERE id_stock=%s",(nuevoNombreFoto,id))
+        cursor.execute("UPDATE `inventario` SET url_foto=%s WHERE id_stock=%s",(nuevoNombreFoto,id))
         conn.commit()
 
     cursor.execute(sql,datos)
